a5e4fc4ecbfb8292d674df7c829284a609a16031
EC2 Default User <ec2-user@ip-172-31-94-29.ec2.internal>
2021-09-09 03:36:13 +0000

    Final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useStorage from './hooks/storage.js';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+function App() {  
+  const { data, findIndex } = useStorage(
+    {    initData: ["Huyen", "Hoa", "Hung", "Long"]  })  
+  const [index, setIndex] = useState(0);  
+  const [name, setName] = useState("");  
+  return (    
+    <div className="App">      
+    <div>学生一覧: [{data.toString()}]      </div>
+    <div>検索名前:        
+      <input  type="text" value={name}  onChange={(e) => setName(e.target.value)}  />        
+      <button  type="button"  onClick={() => setIndex(findIndex(name))} >確定 </button>      
+    </div>      
+    <div>検索名前: {name} </div>      
+    <div>位置: {index}  </div>    
+    </div>  );
+  
+}export default App;
---
+++ src/hooks/storage.js
@@ -0,0 +1,10 @@
+const useStorage = ({ initData }) => {    
+    const data = initData;    
+    const findIndex = (name) => {        
+        return data.indexOf(name) + 1 || NaN;    
+        
+    }   
+    return {data,findIndex};
+    
+};
+export default useStorage
