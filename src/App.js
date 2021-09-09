751c089fb3d1e4158af4c46eedfb16f47e7acdd2
EC2 Default User <ec2-user@ip-172-31-94-29.ec2.internal>
2021-09-09 03:47:40 +0000

    Final

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ function App() {
   const [index, setIndex] = useState(0);  
   const [name, setName] = useState("");  
   return (    
-    <div className="App">      
-    <div>学生一覧: [{data.toString()}]      </div>
-    <div>検索名前:        
-      <input  type="text" value={name}  onChange={(e) => setName(e.target.value)}  />        
-      <button  type="button"  onClick={() => setIndex(findIndex(name))} >確定 </button>      
-    </div>      
+    <div className="panel">
+    <div className="panel-block">      
+      <div>学生一覧: [{data.toString()}]      </div>
+      <div>検索名前:        
+        <input  type="text" value={name}  onChange={(e) => setName(e.target.value)}  />        
+        <button  type="button"  onClick={() => setIndex(findIndex(name))} >確定 </button>      
+      </div>      
     <div>検索名前: {name} </div>      
     <div>位置: {index}  </div>    
-    </div>  );
+    </div>
+    </div>);
   
 }export default App;
